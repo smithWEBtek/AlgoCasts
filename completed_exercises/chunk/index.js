7b11d953969814de76d05b47c06bb8ae6cf4718f
smithwebtek <brad@smithwebtek.com>
2021-01-17 11:24:25 -0500

    Thru chunk

--- completed_exercises/chunk/index.js
+++ completed_exercises/chunk/index.js
@@ -8,32 +8,19 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-  const chunked = [];
-  let index = 0;
+// iterate over arr
+// create an array of n size using the first n available elements
+// push that sub array into the result array
+// if the available elements are less than n, push them into result as an array
+// return the result array
 
-  while (index < array.length) {
-    chunked.push(array.slice(index, index + size));
-    index += size;
+function chunk(arr, size) {
+  const result = []
+  for (let i = 0; i <= arr.length - 1; i += size) {
+    // result.push(Array.from(arr.slice(i, i + size)))
+    result.push(arr.slice(i, i + size))
   }
-
-  return chunked;
+  return result
 }
 
 module.exports = chunk;
-
-// function chunk(array, size) {
-//   const chunked = [];
-//
-//   for (let element of array) {
-//     const last = chunked[chunked.length - 1];
-//
-//     if (!last || last.length === size) {
-//       chunked.push([element]);
-//     } else {
-//       last.push(element);
-//     }
-//   }
-//
-//   return chunked;
-// }
--- completed_exercises/fizzbuzz/index.js
+++ completed_exercises/fizzbuzz/index.js
@@ -12,18 +12,32 @@
 //   4
 //   buzz
 
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     // Is the number a multiple of 3 and 5?
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('fizzbuzz');
+//     } else if (i % 3 === 0) {
+//       // Is the number a multiple of 3?
+//       console.log('fizz');
+//     } else if (i % 5 === 0) {
+//       console.log('buzz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+
 function fizzBuzz(n) {
-  for (let i = 1; i <= n; i++) {
-    // Is the number a multiple of 3 and 5?
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log('fizzbuzz');
-    } else if (i % 3 === 0) {
-      // Is the number a multiple of 3?
-      console.log('fizz');
-    } else if (i % 5 === 0) {
-      console.log('buzz');
+  for(let i = 1; i <= n; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz')
+    } else if(i % 3 === 0) {
+        console.log('fizz')
+    } else if(i % 5 === 0) {
+        console.log('buzz')
     } else {
-      console.log(i);
+        console.log(i)
     }
   }
 }

459a0530c519df9dfa666cdd610e6f6387faa4d3
brad@smithwebtek.com <brad@smithwebtek.com>
2021-03-16 22:53:01 -0400

    Restart algo study

--- completed_exercises/pyramid/index.js
+++ completed_exercises/pyramid/index.js
@@ -14,28 +14,46 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n, row = 0, level = '') {
-  if (row === n) {
-    return;
+function sp(num) {
+  let result = ''
+  for (let i = 0; i < num; i++) {
+    result += '.'
   }
-
-  if (level.length === 2 * n - 1) {
-    console.log(level);
-    return pyramid(n, row + 1);
-  }
-
-  const midpoint = Math.floor((2 * n - 1) / 2);
-  let add;
-  if (midpoint - row <= level.length && midpoint + row >= level.length) {
-    add = '#';
-  } else {
-    add = ' ';
-  }
-  pyramid(n, row, level + add);
+  return result
 }
 
-module.exports = pyramid;
-//
+function mk(num) {
+  let result = ''
+  for (let i = 0; i < num; i++) {
+    result += '#'
+  }
+  return result
+}
+
+function pyramid(n) {
+  let level = ''
+  for (let i = n; i >= 0; i--) {
+    level = sp(i + 1) + mk(n - i) + sp(n + i)
+    console.log(level)
+  }
+}
+
+// pyramid(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1)
 // function pyramid(n) {
 //   const midpoint = Math.floor((2 * n - 1) / 2);
 //
@@ -53,3 +71,26 @@ module.exports = pyramid;
 //     console.log(level);
 //   }
 // }
+
+// 2)
+// function pyramid(n, row = 0, level = '') {
+//   if (row === n) {
+//     return;
+//   }
+
+//   if (level.length === 2 * n - 1) {
+//     console.log(level);
+//     return pyramid(n, row + 1);
+//   }
+
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   let add;
+//   if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//     add = '#';
+//   } else {
+//     add = ' ';
+//   }
+//   pyramid(n, row, level + add);
+// }
+
+module.exports = pyramid;
